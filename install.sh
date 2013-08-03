32641e0555b6c133d73b109bd7326e7245cf5636
Konrad Mrożek <konrad.mrozek@gmail.com>
2013-08-03 13:13:05 +0200

    Add installation script

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+function l() {
+	if [ ! -e ~/$2 ]
+	then
+		echo "Linking $1 to $2"
+		ln -s `pwd`/$1 ~/$2
+	else
+		echo "Already installed $2"
+	fi
+}
+
+l zshrc .zshrc
+l zprofile .zprofile
+l aliases .aliases
+l vimfiles .vim
+l vimrc .vimrc
+l tmux.conf .tmux.conf
+l bin .bin
+
