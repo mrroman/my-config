c01775160b6d3153c008b4c8dbddae83362cfed8
Konrad Mrozek <kmrozek@infusion.com>
2014-02-24 09:39:49 +0100

    Better look of installation script.

--- install.sh
+++ install.sh
@@ -1,32 +1,59 @@
 #!/bin/sh
 
-function l() {
-	if [ ! -e ~/$2 ]
-	then
-		echo "Links: Linking $1 to $2"
-		ln -s `pwd`/$1 ~/$2
-	else
-		echo "Links: Already installed $2"
-	fi
+
+function log()
+{
+    h1color='\e[0;32m'
+    h2color='\e[0;36m'
+    endColor='\e[0m'
+
+    case "$1" in
+        1)
+            echo "${h1color}$2${endColor}: $3"
+            ;;
+        2) 
+            echo "${h2color}$2${endColor}: $3"
+            ;;
+    esac
 }
 
-echo
-echo "Set configuration links:"
-l zshrc .zshrc
-l aliases .aliases
-l vimfiles .vim
-l vimrc .vimrc
-l tmux.conf .tmux.conf
-l bin .bin
+function l() 
+{
+    if [ ! -e ~/$2 ]
+    then
+        log 2 "Links" "Linking $1 to $2"
+        ln -s `pwd`/$1 ~/$2
+    else
+        log 2 "Links" "Already installed $2"
+    fi
+}
 
-echo
-echo "Configure Git:"
-echo "Git: set aliases"
-git config --global alias.co checkout
-git config --global alias.st status
-git config --global alias.ci commit
+function set_links
+{
+    log 1 "Set configuration links"
+    l zshrc .zshrc
+    l aliases .aliases
+    l vimfiles .vim
+    l vimrc .vimrc
+    l tmux.conf .tmux.conf
+    l bin .bin
+}
 
-echo "Git: set credential"
-git config --global credential.helper cache
-git config --global credential.helper 'cache --timeout=3600'
+function git_config
+{
+    log 1 "Configure Git"
+    log 2 "Git" "Set aliases"
+    git config --global alias.co checkout
+    git config --global alias.st status
+    git config --global alias.ci commit
 
+    log 2 "Git" "Set credential"
+    git config --global credential.helper cache
+    git config --global credential.helper 'cache --timeout=3600'
+}
+
+for nm in set_links git_config
+do
+    $nm
+    echo
+done
