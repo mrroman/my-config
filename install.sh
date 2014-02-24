38a4e503ab4a725aea6c36a51ea1dc6c954d6143
Konrad Mrozek <kmrozek@infusion.com>
2014-02-24 09:24:27 +0100

    Add GIT settings

--- install.sh
+++ install.sh
@@ -3,17 +3,30 @@
 function l() {
 	if [ ! -e ~/$2 ]
 	then
-		echo "Linking $1 to $2"
+		echo "Links: Linking $1 to $2"
 		ln -s `pwd`/$1 ~/$2
 	else
-		echo "Already installed $2"
+		echo "Links: Already installed $2"
 	fi
 }
 
+echo
+echo "Set configuration links:"
 l zshrc .zshrc
 l aliases .aliases
 l vimfiles .vim
 l vimrc .vimrc
 l tmux.conf .tmux.conf
 l bin .bin
+
+echo
+echo "Configure Git:"
+echo "Git: set aliases"
+git config --global alias.co checkout
+git config --global alias.st status
+git config --global alias.ci commit
+
+echo "Git: set credential"
+git config --global credential.helper cache
+git config --global credential.helper 'cache --timeout=3600'
 
